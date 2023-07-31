0b8ea954226cb74046f89d82f0917aaba64e2843
Pooyan Kazemian <pooyanunited@gmail.com>
2023-07-31 00:01:36 +0000

    "New Update"

--- rust/homeworks/homework4/functions/functions1.rs
+++ rust/homeworks/homework4/functions/functions1.rs
@@ -1,9 +1,13 @@
 // functions1.rs
 // Make me compile! Execute `zustlings hint functions1` for hints :)
 
-// I AM NOT DONE
 
 
 fn main() {
     call_me();
+}
+
+fn call_me() {
+    let x = 3;
+    println!("X is {x}.")
 }
