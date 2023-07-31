fc598e3f9b937a45d897f046145f1526556bb2bd
Pooyan Kazemian <pooyanunited@gmail.com>
2023-07-31 03:22:59 +0000

    Updated solution

--- rust/homeworks/homework4/primitive_types/primitive_types3.rs
+++ rust/homeworks/homework4/primitive_types/primitive_types3.rs
@@ -2,11 +2,10 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `zustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
-
+    let a = ["Are we there yet?"; 100];
+    a;
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
